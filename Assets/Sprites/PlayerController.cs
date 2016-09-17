e5924f61f96b12b6ef1c9b3069a1bc8201b78511
Shengyi Chen <shengyic@usc.edu>
2016-09-16 23:50:49 -0700

    First Version

---
+++ Assets/Sprites/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;             //Floating point variable to store the player's movement speed.
+
+	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+	private int count;
+
+	public Text countText;
+
+	public Text winText;
+
+	// Use this for initialization
+	void Start()
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+
+	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	void FixedUpdate()
+	{
+		//Store the current horizontal input in the float moveHorizontal.
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		//Store the current vertical input in the float moveVertical.
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+		rb2d.AddForce (movement * speed);
+	}
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() 
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12)
+			winText.text = "You Win!"; 
+	}
+}
